afcd85c283f4d1d3ffb271c1ed6afe6b93af3019
Tayla Ball <tayla.ball@hotmail.com>
2024-11-26 12:25:59 +0000

    Add: home_page to urls.py

--- muddy_paws/settings.py
+++ muddy_paws/settings.py
@@ -19,6 +19,7 @@ if os.path.isfile("env.py"):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -28,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-taylajball-muddypaws-0qr1elsjh33.ws.codeinstitute-ide.net', 'muddy-paws-b5bd6fb9d716.herokuapp.com']
 
@@ -134,7 +135,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

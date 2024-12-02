721208af33ed39dd292028d13fe36e0a5f879202
Tayla Ball <tayla.ball@hotmail.com>
2024-12-02 16:03:27 +0000

    Add: some styling to index.html

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,7 +4,7 @@ from dashboard.views import Dashboard, AddPet, PetDetail
 from booking.views import AddBooking, Bookings, BookingDetail
 
 urlpatterns = [
-    path("dashboard", AddPet.as_view(), name="add_pet"),
+    path("add_pet.html", AddPet.as_view(), name="add_pet"),
     path("", Dashboard.as_view(), name="dashboard"),
     path("dashboard/pet_detail/<slug:pk>/", PetDetail.as_view(), name="pet_detail"),
 ]

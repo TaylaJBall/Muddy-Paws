73de0c579103589b0c8e7b62fce5845306c122b4
Tayla Ball <tayla.ball@hotmail.com>
2024-12-11 10:07:47 +0000

    Add: documentation folder and adjusted button to pass valdation

--- static/js/pets.js
+++
@@ -1,4 +0,0 @@
-function loadPetDetail(petID) {
-    document.getElementById('pet-detail-container').style.display = 'block';
-    document.getElementById('pet-detail-iframe').src = "/dashboard/pet_detail/" + petID + "/";
-}
